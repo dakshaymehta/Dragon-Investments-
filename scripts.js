29f672cb6e656555630047977da28c11790c56f3
Dakshay Mehta <50276213+dakshaymehta@users.noreply.github.com>
2023-03-23 16:46:16 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('form').on('submit', function(event) {
+      event.preventDefault();
+      alert('Form submitted! Thank you for your interest in Dragon Investments.');
+    });
+  });
+
+  
